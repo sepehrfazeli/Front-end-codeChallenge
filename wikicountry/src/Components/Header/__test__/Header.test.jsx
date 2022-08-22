a27f52ceebcfc81d50405b122375c3ca19cfca32
Sepehr Fazeli <sky.f1118@gmail.com>
2022-08-23 00:40:03 +0200

    Header test update (Linked addded to component)

--- wikicountry/src/Components/Header/__test__/Header.test.jsx
+++ wikicountry/src/Components/Header/__test__/Header.test.jsx
@@ -4,7 +4,7 @@ import Header from './../Header';
 import { act } from 'react-dom/test-utils';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 let container;
 
@@ -13,39 +13,49 @@ beforeEach(() => {
   document.body.appendChild(container);
 });
 
-
-
 afterEach(() => {
   document.body.removeChild(container);
   container = null;
 });
 
-
-
 it('should render without crashing', () => {
   // render(<Header />, div);
   act(() => {
-    createRoot(container).render(<Header/>);
+    createRoot(container).render(
+      <Router>
+        <Header />
+      </Router>
+    );
   });
   // ReactDOM.unmountComponentAtNode(div);
   // root.unmount();
 });
 
-
-it('should Header icon be visible without crashing', () => { 
-
-  const {getByTestId} = render(<Header/>);
+it('should Header icon be visible without crashing', () => {
+  const { getByTestId } = render(
+    <Router>
+      <Header />
+    </Router>
+  );
   expect(getByTestId('Header-icon-test')).toBeVisible();
-
 });
 
-it('should render Header h1 correctly', () => { 
-  const {getByTestId} = render(<Header/>);
-  expect(getByTestId('Header-h1-test')).toHaveTextContent('Where in the world?');
+it('should render Header h1 correctly', () => {
+  const { getByTestId } = render(
+    <Router>
+      <Header />
+    </Router>
+  );
+  expect(getByTestId('Header-h1-test')).toHaveTextContent(
+    'Where in the world?'
+  );
 });
 
-
-it('should render Header p correctly', () => { 
-  const {getByTestId} = render(<Header/>);
+it('should render Header p correctly', () => {
+  const { getByTestId } = render(
+    <Router>
+      <Header />
+    </Router>
+  );
   expect(getByTestId('Header-p-test')).toHaveTextContent('Dark Mode');
 });
