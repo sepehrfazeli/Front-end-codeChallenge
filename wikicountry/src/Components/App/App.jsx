cb7014b59a99109be98f588fb95c59abd84bc81a
Sepehr Fazeli <sky.f1118@gmail.com>
2022-08-23 00:42:03 +0200

    App component Updated with new components

--- wikicountry/src/Components/App/App.jsx
+++ wikicountry/src/Components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Results from '../Components-Homepage/Results/Results';
@@ -6,13 +6,22 @@ import Information from '../Components-DetailPage/Information/Information';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+
+  const [cardName, setCardName] = useState('');
+
+  function addCard(card) {
+    setCardName(card.name);
+  }
+  function changeCard(card) {
+    setCardName(card);
+  }
   return (
     <div className="App" data-testid="App-test">
       <Router>
         <Header />
         <Routes>
-          <Route index element={<Results />}></Route>
-          <Route path="/information" element={<Information/>}></Route>
+          <Route index element={<Results onAdd={addCard} />}></Route>
+          <Route path="/information" element={<Information key={cardName} onborder={x => changeCard(x)} card={cardName} />}></Route>
         </Routes>
       </Router>
     </div>
