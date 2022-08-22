ac278036d5fb4f144769b5925c434e81a7d69e00
Sepehr Fazeli <sky.f1118@gmail.com>
2022-08-23 00:34:09 +0200

    added Link to HomePage to header

--- wikicountry/src/Components/Header/Header.jsx
+++ wikicountry/src/Components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Header.css';
 import { UilMoon } from '@iconscout/react-unicons';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="Header" data-testid="Header-test">
       <div className="container">
-        <h1 data-testid="Header-h1-test"> Where in the world? </h1>
+        <Link to="/" >
+          <h1 data-testid="Header-h1-test"> Where in the world? </h1>
+        </Link>
         <button data-testid="Header-button-test">
-          <UilMoon data-testid="Header-icon-test" className="icon"/>
+          <UilMoon data-testid="Header-icon-test" className="icon" />
           <p data-testid="Header-p-test">Dark Mode</p>
         </button>
       </div>
