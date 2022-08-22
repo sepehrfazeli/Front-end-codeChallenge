db34f3ccda075db4fdb604ff03df27960d6b325e
Sepehr Fazeli <sky.f1118@gmail.com>
2022-08-22 22:37:41 +0200

    Results component + test

--- wikicountry/src/Components/Components-Homepage/Results/Results.jsx
+++ wikicountry/src/Components/Components-Homepage/Results/Results.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState,useEffect} from 'react';
 import './Results.css';
+import { API } from '../../../util/API';
+import CardList from '../CardList/CardList';
 
-function Results() {
-  return (
-    <div className='Results'>Results</div>
-  );
+function Results(props) {
+  const [searchResults, setsearchResults] = useState(null);
+
+  useEffect(() => {
+    API.init().then((searchResults) => {
+      setsearchResults(searchResults);
+    });
+  }, []);
+  
+  try {
+    return (
+      <div className="Results">
+        <div className="navbar">
+        </div>
+        <CardList
+          isRemoval={false}
+          onAdd={props.onAdd}
+          cards={searchResults}
+        />
+      </div>
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default Results;
---
+++ wikicountry/src/Components/Components-Homepage/Results/__test__/Results.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Results from '../Results';
+// import { act } from 'react-dom/test-utils';
+// import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+
+let container;
+
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+
+
+it('should render without crashing', () => {
+  // act(() => {
+  createRoot(container).render(<Results />);
+  // });
+});
+
+
